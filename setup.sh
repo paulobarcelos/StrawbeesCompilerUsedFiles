f9326c8533afb0d9b3a4563ca9ac8fa3677f8042
Paulo Barcelos <paulo@azucrina.org>
2018-11-12 17:18:54 +0100

    Added npm

--- setup.sh
+++ setup.sh
@@ -14,19 +14,7 @@ sudo apt-get --yes --force-yes install curl
 sudo apt-get --yes --force-yes install wget
 sudo apt-get --yes --force-yes install strace
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-export NVM_DIR="/root/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-nvm install 11.1.0
-npm --version
-nvm --version
-
-rm -rf node_modules
-npm install quirkbot-arduino-builder
-npm install quirkbot-arduino-hardware
-npm install quirkbot-arduino-library
-npm install quirkbot-avr-gcc
+npm install
 
 # create temp dir and setup files
 echo "#########################################################################"
